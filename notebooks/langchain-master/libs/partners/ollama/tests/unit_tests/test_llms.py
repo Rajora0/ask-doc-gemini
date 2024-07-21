8607028a15ea0bfced72c219aca802f2aa7b998f
Rajora0 <rafaelj492@gmail.com>
2024-07-21 13:08:57 -0300

    Database: Creating the Chroma vector database

--- notebooks/langchain-master/libs/partners/ollama/tests/unit_tests/test_llms.py
+++
@@ -1,8 +0,0 @@
-"""Test Ollama Chat API wrapper."""
-
-from langchain_ollama import OllamaLLM
-
-
-def test_initialization() -> None:
-    """Test integration initialization."""
-    OllamaLLM(model="llama3")
